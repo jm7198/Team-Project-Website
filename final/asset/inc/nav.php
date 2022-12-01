4a0e483cfd5a5d82a7b2595732051d40eb89709c
Raghav2178 <89413361+Raghav2178@users.noreply.github.com>
2022-11-30 22:36:37 -0500

    Hamburger
    Files

---
+++ final/asset/inc/nav.php
@@ -0,0 +1,13 @@
+<nav>
+    <ul class="menu">
+        <li><a class="menuItem" href="#">Home</a></li>
+        <li><a class="menuItem" href="#">Profile</a></li>
+        <li><a class="menuItem" href="#">About</a></li>
+        <li><a class="menuItem" href="#">Contacts</a></li>
+    </ul>
+    <button class="hamburger">
+        <!-- material icons https://material.io/resources/icons/ -->
+        <i class="menuIcon material-icons">menu</i>
+        <i class="closeIcon material-icons">close</i>
+    </button>
+</nav>
