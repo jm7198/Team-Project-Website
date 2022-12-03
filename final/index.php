7b2d11c145a6788b3a14246c64ebf93dc0b6e116
John Mihal <69979814+jm7198@users.noreply.github.com>
2022-12-03 15:41:10 -0500

    added solar system planets
    Im not done, I am working on putting stuff on the page, and then later I will move it around.

--- final/index.php
+++ final/index.php
@@ -3,15 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Humburger Menu</title>
+    <title>The Solar System</title>
+    <link rel="stylesheet" href="asset/css/home.css">
     <link rel="stylesheet" href="asset/css/nav.css">
-    <link rel="stylesheet" href="asset/css/style.css">
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
 </head>
 <body>
 
     <?php include 'asset/inc/nav.php';?>
     <?php include 'asset/inc/header.php';?>
+
+    <div id="solar-system">
+      <a id="sun" href="asset/subpages/sun.php"><img id="sun-img" src="asset/images/sun.png" alt="image of the sun"></a>
+      <a id="mercury" href="asset/subpages/mercury.php"><img id="mercury-img" src="asset/images/mercury.png" alt="image of the mercury"></a>
+      <a id="venus" href="asset/subpages/venus.php"><img id="venus-img" src="asset/images/venus.png" alt="image of the venus"></a>
+      <a id="earth" href="asset/subpages/earth.php"><img id="earth-img" src="asset/images/earth.png" alt="image of the earth"></a>
+      <a id="mars" href="asset/subpages/mars.php"><img id="mars-img" src="asset/images/mars.png" alt="image of the mars"></a>
+      <a id="jupiter" href="asset/subpages/jupiter.php"><img id="jupiter-img" src="asset/images/jupiter.png" alt="image of the jupiter"></a>
+      <a id="saturn" href="asset/subpages/saturn.php"><img id="saturn-img" src="asset/images/saturn.png" alt="image of the saturn"></a>
+      <a id="uranus" href="asset/subpages/uranus.php"><img id="uranus-img" src="asset/images/uranus.png" alt="image of the uranus"></a>
+      <a id="neptune" href="asset/subpages/neptune.php"><img id="neptune-img" src="asset/images/neptune.png" alt="image of the neptune"></a>
+    </div>
+
     <script src="asset/js/hamburger.js"></script>
 </body>
 </html
