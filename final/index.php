3a8eb90a371074514e9b25c2f6b7613799589702
John Mihal <69979814+jm7198@users.noreply.github.com>
2022-12-05 01:48:16 -0500

    added footer to home page

--- final/index.php
+++ final/index.php
@@ -11,26 +11,25 @@
 </head>
 <body>
 
-    <?php include 'asset/inc/nav.php';?>
-    <?php include 'asset/inc/header.php';?>
-    
+  <?php include 'asset/inc/nav.php';?>
+  <?php include 'asset/inc/header.php';?>
+
+  <div id="page-container">
     <h2 id="planet-select">select a planet</h2>
-    
-
-    <div id="solar-system">
-      <div><a id="sun" href="asset/subpages/sun.php"><img id="sun-img" src="asset/images/sun.png" alt="image of the sun"></a></div>
-      <div><a id="mercury" href="asset/subpages/mercury.php"><img id="mercury-img" src="asset/images/mercury.png" alt="image of the mercury"></a></div>
-      <div><a id="venus" href="asset/subpages/venus.php"><img id="venus-img" src="asset/images/venus.png" alt="image of the venus"></a></div>
-      <div><a id="earth" href="asset/subpages/earth.php"><img id="earth-img" src="asset/images/earth.png" alt="image of the earth"></a></div>
-      <div><a id="mars" href="asset/subpages/mars.php"><img id="mars-img" src="asset/images/mars.png" alt="image of the mars"></a></div>
-      <div><a id="jupiter" href="asset/subpages/jupiter.php"><img id="jupiter-img" src="asset/images/jupiter.png" alt="image of the jupiter"></a></div>
-      <div><a id="saturn" href="asset/subpages/saturn.php"><img id="saturn-img" src="asset/images/saturn.png" alt="image of the saturn"></a></div>
-      <div><a id="uranus" href="asset/subpages/uranus.php"><img id="uranus-img" src="asset/images/uranus.png" alt="image of the uranus"></a></div>
-      <div><a id="neptune" href="asset/subpages/neptune.php"><img id="neptune-img" src="asset/images/neptune.png" alt="image of the neptune"></a></div>
-    </div>
-
-    <script src="asset/js/hamburger.js"></script>
-    <script src="asset/js/home.js"></script>
-
+      <div id="solar-system">
+        <div><a id="sun" href="asset/subpages/sun.php"><img id="sun-img" src="asset/images/sun.png" alt="image of the sun"></a></div>
+        <div><a id="mercury" href="asset/subpages/mercury.php"><img id="mercury-img" src="asset/images/mercury.png" alt="image of the mercury"></a></div>
+        <div><a id="venus" href="asset/subpages/venus.php"><img id="venus-img" src="asset/images/venus.png" alt="image of the venus"></a></div>
+        <div><a id="earth" href="asset/subpages/earth.php"><img id="earth-img" src="asset/images/earth.png" alt="image of the earth"></a></div>
+        <div><a id="mars" href="asset/subpages/mars.php"><img id="mars-img" src="asset/images/mars.png" alt="image of the mars"></a></div>
+        <div><a id="jupiter" href="asset/subpages/jupiter.php"><img id="jupiter-img" src="asset/images/jupiter.png" alt="image of the jupiter"></a></div>
+        <div><a id="saturn" href="asset/subpages/saturn.php"><img id="saturn-img" src="asset/images/saturn.png" alt="image of the saturn"></a></div>
+        <div><a id="uranus" href="asset/subpages/uranus.php"><img id="uranus-img" src="asset/images/uranus.png" alt="image of the uranus"></a></div>
+        <div><a id="neptune" href="asset/subpages/neptune.php"><img id="neptune-img" src="asset/images/neptune.png" alt="image of the neptune"></a></div>
+      </div>
+  </div>
+  <?php include 'asset/inc/footer.php';?>
+  <script src="asset/js/hamburger.js"></script>
+      <script src="asset/js/home.js"></script>
 </body>
 </html
