0a8a95c7d48eb27e1c20edec3012312a9595b5fa
John Mihal <69979814+jm7198@users.noreply.github.com>
2022-12-05 00:08:13 -0500

    fixed nav mixup

--- final/asset/inc/nav2.php
+++ final/asset/inc/nav2.php
@@ -1,26 +1,30 @@
 
-<nav>
+
+        <nav>
             <!-- <div class="logo"><h1>logo</h1></div>  -->
             <div class="openMenu"><i class="fa fa-bars"></i></div>
             <ul class="mainMenu">
-                <li><a href="../../">Home</a></li>
+                <li><a href="../../index.php">Home</a></li>
                 <div class="dropdown">
                     <button class="dropbtn">PLANETS</button>
                     <div class="dropdown-content">
-                    <a href="sun.php">Sun</a>
-                    <a href="mercury.php">Mercury</a>
-                    <a href="venus.php">Venus</a>
-                    <a href="earth.php">Earth</a>
-                    <a href="mars.php">Mars</a>
-                    <a href="jupiter.php">Jupiter</a>
-                    <a href="saturn.php">Saturn</a>
-                    <a href="uranus.php">Uranus</a>
-                    <a href="neptune.php">Neptune</a>
+                    <a href="asset/subpages/mercury.php">Mercury</a>
+                    <a href="asset/subpages/venus.php">Venus</a>
+                    <a href="asset/subpages/earth.php">Earth</a>
+                    <a href="asset/subpages/mars.php">Mars</a>
+                    <a href="asset/subpages/jupiter.php">Jupiter</a>
+                    <a href="asset/subpages/saturn.php">Saturn</a>
+                    <a href="asset/subpages/uranus.php">Uranus</a>
+                    <a href="asset/subpages/neptune.php">Neptune</a>
                  
                     </div>
                 </div>
-                <li><a href="#">Blank</a></li>
-                <li><a href="#">blank</a></li>
+                <li id="filler"></li>
+                <li class="page-nav"><a href="#Description">Description</a></li>
+                <li class="page-nav"><a href="#Fun-Fact">Fun-Facts</a></li>
+                <li class="page-nav"><a href="#Atmostphere">Atmostphere</a></li>
+                <li class="page-nav"><a href="#Geology">Geology</a></li>
+
                 <div class="closeMenu"><i class="fa fa-times"></i></div>
                 <span class="icons">
                     <i class="fab fa-facebook"></i>
