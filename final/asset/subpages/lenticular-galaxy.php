1b4294b89ad551915db0e1a1187e45d1a815e67a
John Mihal <69979814+jm7198@users.noreply.github.com>
2022-12-04 23:03:35 -0500

    added extra solar entities pages

---
+++ final/asset/subpages/lenticular-galaxy.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/planets.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <title>Thorne–Żytkow object</title>
+</head>
+
+<body>
+    <?php include '../inc/nav.php';?>
+    <header>
+        <h1>Lenticular Galaxy</h1>
+    </header>
+    <img src="../images/galaxy.png" alt="A image of a Lenticular Galaxy">
+    <div class="container">
+        <section id="Description">
+            <h2>Description</h2>
+            <p>
+                A lenticular galaxy (denoted S0) is a type of galaxy intermediate between an elliptical (denoted E) and a spiral galaxy in galaxy morphological classification schemes. It contains a large-scale disc but does not have large-scale spiral arms. Lenticular galaxies are disc galaxies that have used up or lost most of their interstellar matter and therefore have very little ongoing star formation. They may, however, retain significant dust in their disks. As a result, they consist mainly of aging stars (like elliptical galaxies). Despite the morphological differences, lenticular and elliptical galaxies share common properties like spectral features and scaling relations. Both can be considered early-type galaxies that are passively evolving, at least in the local part of the Universe. Connecting the E galaxies with the S0 galaxies are the ES galaxies with intermediate-scale discs.        </section>
+            </p>
+        <section id="Fun-Fact">
+            <h2>Fun Facts</h2>
+            <ul>
+                <li>
+                Lenticular galaxies are often considered to be a poorly understood transition state between spiral and elliptical galaxies.
+                </li>
+                <li>Distance From earth: Lenticular Galaxies are a type of galaxy. One such galaxy NGC 2787 is located around 24 million light years away.</li>
+            </ul>
+        </section>
+    </div>
+
+</body>
+
+</html>
---
+++ final/asset/subpages/throne-zytkow.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/planets.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <title>Thorne–Żytkow object</title>
+</head>
+
+<body>
+    <?php include '../inc/nav.php';?>
+    <header>
+        <h1>Thorne–Żytkow object</h1>
+    </header>
+    <img src="../images/thornezytkow.png" alt="A illustration of a Thorne–Żytkow object">
+
+    <div class="container">
+        <section id="Description">
+            <h2>Description</h2>
+            <p>A Thorne–Żytkow object (TŻO or TZO), also known as a hybrid star, is a conjectured type of star wherein a red giant or red supergiant contains a neutron star at its core, formed from the collision of the giant with the neutron star. Such objects were hypothesized by Kip Thorne and Anna Żytkow in 1977. In 2014, it was discovered that the star HV 2112, located in the Small Magellanic Cloud (SMC), was a strong candidate. Another possible candidate is the star HV 11417, also located in the SMC.</p>
+        </section>
+
+        <section id="Fun-Fact">
+            <h2>Fun Fact</h2>
+            <ul>
+                <li>A TŻO has an estimated lifespan of 105–106 years. Given this lifespan, it is possible that between 20 and 200 Thorne-Żytkow objects currently exist in the Milky Way.</li>
+                <li>Distance From earth: Not applicable because this is semi theoretical and unproven, though evidence does exist for them.</li>
+            </ul>
+        </section>
+    </div>
+
+</body>
+
+</html>
